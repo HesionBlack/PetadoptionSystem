426dd3a6f041c2fd5d23c5dd9dec5269db2e6f4e
HesionBlack <1026320617@qq.com>
2019-12-18 17:27:21 +0800

    修复一些细节

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/PetApply.java
@@ -0,0 +1,21 @@
+package com.ruoyi.system.domain;/**
+ * ClassName: PetApply <br/>
+ * Description: <br/>
+ * date: 2019/12/18 下午4:56<br/>
+ *
+ * @author Hesion<br />
+ * @version
+ * @since JDK 1.8
+ */
+
+/**
+ * @program: ruoyi
+ *
+ * @description:
+ *
+ * @author: hesion
+ *
+ * @create: 2019-12-18 16:56
+ **/
+public class PetApply {
+}
